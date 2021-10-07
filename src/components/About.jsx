11dd6a8cd93d6e80c2ee33d09612ff4d4103b347
harlexander <dunkwualex6@gmail.com>
2021-10-07 20:33:58 +0100

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             </div>
             <div className='col-xs-12 col-md-6'>
               <div className='about-text text-left'>
-                  <Fade top>
+                  <Fade bottom>
                      <h2>About Us</h2>
                 <div className='list-style row text-left px-2'>
                     <p>Over the years, thousands of Nigerian students have been doing great works in our Tertiary Institutions. Helping make life better for themselves and other university students across the country.<br/><br/>Students icon awards is here to award great Nigerian students and appreciate them for the courage and works they've put out in their various Tertiary Institutions.<br/><br/>We are currently going to be covering schools in Edo state, Nigeria. And as time goes by, we'd be covering schools across the country(NIGERIA).</p>
