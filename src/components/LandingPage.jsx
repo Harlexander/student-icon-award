34570eea322a13bd40a49c995c0f2df0d7128079
harlexander <dunkwualex6@gmail.com>
2021-10-07 20:38:20 +0100

    hello

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LandingPage = ({src}) => {
     return (
-        <div className="h-100">
+        <div className="h-100 landing" >
             <img src={"img/"+src} width="100%"/>
         </div>
     )
