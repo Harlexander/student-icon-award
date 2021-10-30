11d104890f7da90236b23b26eadb4c36cdb86956
harlexander <dunkwualex6@gmail.com>
2021-10-30 08:23:38 +0100

    update

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,7 @@
 export const Navigation = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light px-md-5">
-  <a href="#"><img src="img/title.jpg" class="navbar-brand shadow"/></a>
+  <Link to="#"><img src="img/title.jpg" class="navbar-brand shadow"/></Link>
   <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -9,13 +9,13 @@ export const Navigation = () => {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/nomination">Nomination</a>
+        <Link class="nav-link" to="/nomination">Nomination</Link>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/voting">Voting</a>
+        <Link class="nav-link" to="/voting">Voting</Link>
       </li>
     </ul>
   </div>
--- src/pages/Voting.jsx
+++ src/pages/Voting.jsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState } from 'react'
 import LandingPage from '../components/LandingPage'
 import Menu from '../components/Menu'
 import { Navigation } from '../components/navigation'
-import { firestore } from '../firebase/firebase'
 
 const Voting = ({data}) => {
     return (
