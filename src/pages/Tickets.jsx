ae61fe154d2701c441907173d237174c5d57ab51
harlexander <dunkwualex6@gmail.com>
2021-12-25 21:48:49 +0100

    testing

--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -3,16 +3,16 @@ import { Button } from 'react-bootstrap'
 import { Contact } from '../components/contact'
 import LandingPage from '../components/LandingPage'
 import { Navigation } from '../components/navigation'
-// import {PDFDownloadLink} from '@react-pdf/renderer';
-// import Ticketing from '../components/Ticketing'
-// import MyDocument from '../components/PdfTicket'
+import {PDFDownloadLink} from '@react-pdf/renderer';
+import Ticketing from '../components/Ticketing'
+import MyDocument from '../components/PdfTicket'
 
 const Tickets = () => {
     return (
         <div>
             <Navigation/>
             <LandingPage src={"img/coming.jpg"}/>
-            {/* <Ticketing/> */}
+            <Ticketing/>
             <Contact/>
         </div>
     )
